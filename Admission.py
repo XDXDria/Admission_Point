05c54b198c1cc41a3b24b10822b8f734a77f88b3
XDXDria <s6070020@kmitl.ac.th>
2014-12-16 23:05:20 +0700

    แก้ไขการใช้เทียบคะแนน

--- Admission.py
+++ Admission.py
@@ -103,319 +103,315 @@ def get():
         pat2 = nlis[12]*15
         pat3 = nlis[13]*20
         ans = thai+social+math+science+physical+art+work+gpa+gat+pat2+pat3
+        if nlis[0] == 2024:
+            if ans >= 19055.35:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2025:
+            if ans >= 17602.5:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2026:
+            if ans >= 15965.9:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2027:
+            if ans >= 17904.45:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2028:
+            if ans >= 18131.4:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2029:
+            if ans >= 17647.15:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2030:
+            if ans >= 15795.3:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2031:
+            if ans >= 16556.5:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2032:
+            if ans >= 18002.05:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2033:
+            if ans >= 17737.5:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2034:
+            if ans >= 16298.65:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2035:
+            if ans >= 16861.95:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2036:
+            if ans >= 17148.75:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2037:
+            if ans >= 16302.95:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2038:
+            if ans >= 17170.7:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2039:
+            if ans >= 16646.25:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2040:
+            if ans >= 18353.75:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2041:
+            if ans >= 17175.7:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2042:
+            if ans >= 17499.55:
+                print "PASS"
+            else:
+                print "NOT PASS"
     elif nlis[0] == 2043:
         gat = nlis[10]*30
         pat7 = nlis[17]*20
         ans = thai+social+math+science+physical+art+work+gpa+gat+pat7
+        if ans >= 16876.95:
+            print "PASS"
+        else:
+            print "NOT PASS"
     elif nlis[0] == 2044:
         gat = nlis[10]*50
         ans = thai+social+math+science+physical+art+work+gpa+gat
+        if ans >= 19904.45:
+            print "PASS"
+        else:
+            print "NOT PASS"
     elif nlis[0] == 2045:
         gat = nlis[10]*10
         pat3 = nlis[13]*20
         pat5 = nlis[15]*20
         ans = thai+social+math+science+physical+art+work+gpa+gat+pat3+pat5
+        if ans >= 14999.45:
+            print "PASS"
+        else:
+            print "NOT PASS"
     elif nlis[0] >= 2046 and nlis[0] <= 2048:
         gat = nlis[10]*10
         pat4 = nlis[14]*20
         pat5 = nlis[15]*20
         ans = thai+social+math+science+physical+art+work+gpa+gat+pat4+pat5
+        if nlis[0] == 2046:
+            if ans >= 15811.25:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2047:
+            if ans >= 16136.3:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2048:
+            if ans >= 15420:
+                print "PASS"
+            else:
+                print "NOT PASS"
     elif nlis[0] == 2049:
         gat = nlis[10]*10
         pat2 = nlis[12]*20
         pat5 = nlis[15]*20
         ans = thai+social+math+science+physical+art+work+gpa+gat+pat2+pat5
+        if ans >= 13261:
+            print "PASS"
+        else:
+            print "NOT PASS"
     elif nlis[0] >= 2050 and nlis[0] <= 2067:
         gat = nlis[10]*10
         pat1 = nlis[11]*10
         pat2 = nlis[12]*30
         ans = thai+social+math+science+physical+art+work+gpa+gat+pat1+pat2
+        if nlis[0] == 2050:
+            if ans >= 8968.25:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2051:
+            if ans >= 12505.35:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2052:
+            if ans >= 11939.35:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2053:
+            if ans >= 11702.6:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2054:
+            if ans >= 12400:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2055:
+            if ans >= 13133.2:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2056:
+            if ans >= 13839.35:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2057:
+            if ans >= 14271.3:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2058:
+            if ans >= 14277.55:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2059:
+            if ans >= 13933.8:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2060:
+            if ans >= 11243.2:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2061:
+            if ans >= 13724.5:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2062:
+            if ans >= 13213.2:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2063:
+            if ans >= 12953.25:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2064:
+            if ans >= 14944.5:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2065:
+            if ans >= 13168.8:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2066:
+            if ans >= 13852.55:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2067:
+            if ans >= 14337.55:
+                print "PASS"
+            else:
+                print "NOT PASS"
     elif nlis[0] == 2068:
         gat = nlis[10]*15
         pat2 = nlis[12]*15
         pat3 = nlis[13]*20
         ans = thai+social+math+science+physical+art+work+gpa+gat+pat2+pat3
+        if ans >= 17757.85:
+            print "PASS"
+        else:
+            print "NOT PASS"
     elif nlis[0] >= 2069 and nlis[0] <= 2071:
         gat = nlis[10]*30
         pat1 = nlis[11]*20
         ans = thai+social+math+science+physical+art+work+gpa+gat+pat1
+        if nlis[0] == 2069:
+            if ans >= 15257.5:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2070:
+            if ans >= 15497.45:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2071:
+            if ans >= 15943.2:
+                print "PASS"
+            else:
+                print "NOT PASS"
     elif nlis[0] >= 2072 and nlis[0] <= 2074:
         gat = nlis[10]*10
         pat1 = nlis[11]*10
         pat2 = nlis[12]*30
         ans = thai+social+math+science+physical+art+work+gpa+gat+pat1+pat2
+        if nlis[0] == 2072:
+            if ans >= 10783.2:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2073:
+            if ans >= 11013.2:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2074:
+            if ans >= 9789.4:
+                print "PASS"
+            else:
+                print "NOT PASS"
     elif nlis[0] >= 2075 and nlis[0] <= 2076:
         gat = nlis[10]*15
         pat2 = nlis[12]*15
         pat3 = nlis[13]*20
         ans = thai+social+math+science+physical+art+work+gpa+gat+pat2+pat3
+        if nlis[0] == 2075:
+            if ans >= 11161.25:
+                print "PASS"
+            else:
+                print "NOT PASS"
+        elif nlis[0] == 2076:
+            if ans >= 10295.7:
+                print "PASS"
+            else:
+                print "NOT PASS"
 
     print 'Your admission point is '+str(ans)+' Points.'
 
-    if pass_faculty == 2024:
-        if ans >= 19055.35:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2025:
-        if ans >= 17602.5:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2026:
-        if ans >= 15965.9:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2027:
-        if ans >= 17904.45:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2028:
-        if ans >= 18131.4:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2029:
-        if ans >= 17647.15:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2030:
-        if ans >= 15795.3:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2031:
-        if ans >= 16556.5:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2032:
-        if ans >= 18002.05:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2033:
-        if ans >= 17737.5:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2034:
-        if ans >= 16298.65:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2035:
-        if ans >= 16861.95:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2036:
-        if ans >= 17148.75:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2037:
-        if ans >= 16302.95:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2038:
-        if ans >= 17170.7:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2039:
-        if ans >= 16646.25:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2040:
-        if ans >= 18353.75:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2041:
-        if ans >= 17175.7:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2042:
-        if ans >= 17499.55:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2043:
-        if ans >= 16876.95:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2044:
-        if ans >= 19904.45:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2045:
-        if ans >= 14999.45:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2046:
-        if ans >= 15811.25:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2047:
-        if ans >= 16136.3:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2048:
-        if ans >= 15420:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2049:
-        if ans >= 13261:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2050:
-        if ans >= 8968.25:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2051:
-        if ans >= 12505.35:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2052:
-        if ans >= 11939.35:
-            print "PASS"
-        else:
-    elif pass_faculty == 2053:
-        if ans >= 11702.6:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2054:
-        if ans >= 12400:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2055:
-        if ans >= 13133.2:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2056:
-        if ans >= 13839.35:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2057:
-        if ans >= 14271.3:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2058:
-        if ans >= 14277.55:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2059:
-        if ans >= 13933.8:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2060:
-        if ans >= 11243.2:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2061:
-        if ans >= 13724.5:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2062:
-        if ans >= 13213.2:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2063:
-        if ans >= 12953.25:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2064:
-        if ans >= 14944.5:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2065:
-        if ans >= 13168.8:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2066:
-        if ans >= 13852.55:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2067:
-        if ans >= 14337.55:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2068:
-        if ans >= 17757.85:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2069:
-        if ans >= 15257.5:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2070:
-        if ans >= 15497.45:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2071:
-        if ans >= 15943.2:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2072:
-        if ans >= 10783.2:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2073:
-        if ans >= 11013.2:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2074:
-        if ans >= 9789.4:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2075:
-        if ans >= 11161.25:
-            print "PASS"
-        else:
-            print "NOT PASS"
-    elif pass_faculty == 2076:
-        if ans >= 10295.7:
-            print "PASS"
-        else:
-            print "NOT PASS"
             
 Button(master, text="Submit",command = get).grid(row=9,column=1)
 Button(master, text="Close",command=master.destroy).grid(row=9,column=2)
